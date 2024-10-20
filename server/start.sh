94d3c7673db15164215986d4c8c98cc49c741140
piguide.dev <nhatco@gmail.com>
2024-10-20 23:01:11 +0700

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# python3.6 manage.py crontab remove
-# python3.6 manage.py crontab add
+# python3.8 manage.py crontab remove
+# python3.8 manage.py crontab add
 
 # systemctl restart crond.service
 
-echo "Start Django server"
-python3 manage.py collectstatic --noinput
+
 python3 manage.py runserver --insecure 0.0.0.0:80 &
